ab2cd1448a46736ac4616b059e4809dbba332e00
Dylan <120477274@umail.ucc.ie>
2022-12-01 15:17:32 +0000

    added a image for the battery showing it charging and discharging

--- interface.py
+++ interface.py
@@ -1,28 +1,45 @@
 import tkinter as tk
-from tkinter import ttk
 from labels import Labels
 from graph import Graph
 from battery_simulated import Battery
 from tracker import Tracker
+from battery_image import Battery_Image
 
 class GUI(object):
+    '''
+    GUI class creates a graphical interface for the user
+    it provides the current battery level(percentage)
+    users can swap modes using a button
+    users can press a button to show what is happening to the battery on a graph
+    '''
     def __init__(self, battery: Battery, tracker: Tracker):
+        '''
+        self._tracker
+        self._battery
+        self._stop
+        self._root
+        self._labels
+        self._battery_levels
+        self._selected
+        '''
         self._tracker = tracker
         self._battery = battery
         self._stop = 0
         self._root = tk.Tk()
         self._root.title("Battery Optimizer Interface")
-        self._root.geometry("500x500")
-        self._labels = Labels(self._root, self._battery)
-        self._batteryLevels = []
-        self.generatePercentages()
-        self._selected = tk.IntVar(self._root, self._batteryLevels[0])
-        self._labels.setSelected(self._batteryLevels[0])
+        self._root.geometry("350x225")
+        self._battery_image = Battery_Image(self._root)
+        self._labels = Labels(self._root, self._battery, self._battery_image)
+        self._battery_levels = []
+        self.generate_percentages() #calls a method to generate numbers for self._battery_levels
+        self._selected = tk.IntVar(self._root, self._battery_levels[0])
+        self._labels.setSelected(self._battery_levels[0])
 
-        self._modeButton = tk.Button(self._root, text="Modes", font=15, command=lambda: self.modes()).grid(row=1, column=0)
-        self._graphButton = tk.Button(self._root, text="Show on Graph", font=15, command=lambda: self.graph()).grid(row=5, column=0)
-        self._menu = tk.OptionMenu(self._root, self._selected, *self._batteryLevels, command=lambda x: self.setSelectedThreshold())
-        self._menu.configure(font=15)
+        self._mode_button = tk.Button(self._root, text="Modes", font=15, command=lambda: self.modes()).grid(row=1, column=0, padx=10, pady=5)
+        self._graph_button = tk.Button(self._root, text="Show on Graph", font=15, command=lambda: self.graph()).grid(row=5, column=0, padx=15, pady=5)
+
+        self._menu = tk.OptionMenu(self._root, self._selected, *self._battery_levels, command=lambda x: self.setSelectedThreshold())
+        self._menu.configure(font=15, padx=10, pady=5)
         self._menu['menu'].configure(font=15)
         self._root.mainloop()
 
@@ -30,9 +47,9 @@ class GUI(object):
         self._labels.setSelected(self._selected.get())
         self._battery.setUpperThreshold(self._selected.get())
 
-    def generatePercentages(self):
+    def generate_percentages(self):
         for i in range (100, -10, -10):
-            self._batteryLevels.append(i)
+            self._battery_levels.append(i)
 
     def modes(self):
         if self._tracker.get_mode() == "Bypass":
@@ -40,12 +57,16 @@ class GUI(object):
             self._labels.setSelectedMode("Auto")
             self._labels.deleteLabelsForThreshold()
             self._menu.grid_remove()
+            self._root.geometry("350x225")
+            self._battery_image.interfaceAutoGrid()
         else:
             self._tracker.set_bypass()
             self._labels.setSelectedMode("Bypass")
             self._menu.grid(row=3, column=0)
             self._labels.createLabelsForThreshold()
-            self._selected.set(self._batteryLevels[0])
+            self._selected.set(self._battery_levels[0])
+            self._root.geometry("400x300")
+            self._battery_image.interfaceBypassGrid()
 
     def graph(self):
         g = Graph(self._battery, self._tracker)
--- labels.py
+++ labels.py
@@ -1,14 +1,16 @@
 import tkinter as tk
+from battery_image import Battery_Image
 
 class Labels(object):
-    def __init__(self, root, battery):
+    def __init__(self, root, battery, battery_image):
         self._root = root
         self._battery = battery
-        tk.Label(self._root, text="Battery Level: ", font=15).grid(row=0, column=0)
+        self._battery_image = battery_image
+        tk.Label(self._root, text="Battery Level: ", font=15).grid(row=0, column=0, padx=10, pady=10)
         self._selectedMode = tk.StringVar()
         self._selectedMode.set("Auto")
-        tk.Label(self._root, text="Current Mode:", font=15).grid(row=2, column=0)
-        tk.Label(self._root, textvariable=self._selectedMode, font=15).grid(row=2, column=1)
+        tk.Label(self._root, text="Current Mode:", font=15).grid(row=2, column=0, padx=10, pady=10)
+        tk.Label(self._root, textvariable=self._selectedMode, font=15).grid(row=2, column=1, pady=5)
 
 
         self._selected = tk.IntVar()
@@ -25,6 +27,7 @@ class Labels(object):
     def loadBatteryLevel(self):
         self._level.set(self._battery.getLevel())
         self._labelBattery.config(textvariable=self._level)
+        self._battery_image.setCells(self._battery.getLevel())
         self._labelBattery.after(1000, self.loadBatteryLevel)
 
 
@@ -37,8 +40,8 @@ class Labels(object):
         self._label2.grid_remove()
 
     def createLabelsForThreshold(self):
-        self._label1.grid(row=4, column=0)
-        self._label2.grid(row=4, column=1)
+        self._label1.grid(row=4, column=0, padx=10, pady=10)
+        self._label2.grid(row=4, column=1, padx=10, pady=10)
         self.setSelected(100)
 
     def setSelectedMode(self, mode):
